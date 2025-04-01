4c570555228ae91aa124b7fd14309e1303df9ad5
daveycodez <david@swiftapk.com>
2025-04-01 14:48:07 -0700

    Latest Packages

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -39,7 +39,7 @@ function RootComponent() {
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <head>
                 <HeadContent />
             </head>
