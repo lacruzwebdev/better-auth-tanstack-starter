42bf3fccf38dc46262144c6764f085afe6dbd666
daveycodez <david@swiftapk.com>
2025-04-04 13:18:13 -0700

    Powered by better-auth.

--- app/routes/auth/$pathname.tsx
+++ app/routes/auth/$pathname.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 import { AuthCard } from "@daveyplate/better-auth-ui"
 import { createFileRoute } from "@tanstack/react-router"
 
@@ -9,8 +10,25 @@ function RouteComponent() {
     const { pathname } = Route.useParams()
 
     return (
-        <main className="flex grow flex-col items-center justify-center p-4">
+        <main className="flex grow flex-col items-center justify-center gap-4 p-4">
             <AuthCard pathname={pathname} />
+
+            <p
+                className={cn(
+                    ["callback", "settings", "sign-out"].includes(pathname) && "hidden",
+                    "text-muted-foreground text-xs"
+                )}
+            >
+                Powered by{" "}
+                <a
+                    className="text-warning underline"
+                    href="https://better-auth.com"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    better-auth.
+                </a>
+            </p>
         </main>
     )
 }
